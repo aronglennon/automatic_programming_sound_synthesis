9f9da423d20d3759e69472acf6c9d05a0db37b94
Aron Glennon <aronglennon@gmail.com>
2013-03-02 21:44:59 -0500

    Adding DTW example from rpy to work from.

---
+++ src/similarity/dtw.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+import rpy2.robjects.numpy2ri
+from rpy2.robjects.packages import importr
+
+# Set up our R namespaces
+R = rpy2.robjects.r
+DTW = importr('dtw')
+
+# Generate our data
+idx = np.linspace(0, 2*np.pi, 100)
+template = np.cos(idx)
+query = np.sin(idx) + np.array(R.runif(100))/10
+
+# Calculate the alignment vector and corresponding distance
+alignment = R.dtw(query, template, keep=True)
+dist = alignment.rx('distance')[0][0]
+
+print(dist)
