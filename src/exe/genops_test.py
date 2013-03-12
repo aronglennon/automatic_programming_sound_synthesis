85209b3ce55bf02f4f63a9dfa22073c0e29dd4b4
Aron Glennon <aronglennon@gmail.com>
2013-03-12 06:07:01 -0400

    exe: Added placeholders for separate tests.

---
+++ src/exe/genops_test.py
@@ -0,0 +1,5 @@
+'''
+Generate 100 random patches
+Perform each gen op on these patches in 5 different ways
+Calculate NSC
+'''
---
+++ src/exe/similarity_test.py
@@ -0,0 +1,4 @@
+'''
+Generate N different tests of pairs of similar timbral content of various complexity and different content of various complexity.
+Test distances given by each similarity measure to determine which is consistently good for both similar and dissimilar test pairs.
+'''
