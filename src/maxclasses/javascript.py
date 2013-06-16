84498e51d0c127d432ee73e3c5c9e1c3bbf16a35
Aron Glennon <aronglennon@gmail.com>
2013-06-16 11:29:56 -0400

    src: Updated the js generator to work better with record~

--- src/maxclasses/javascript.py
+++ src/maxclasses/javascript.py
@@ -5,7 +5,7 @@ def fill_JS_file(filename, patch, patch_type = 'processing'):
     js_file = open(filename, 'w')
     # Set the number of inlets and outlets
     js_file.write('inlets = 1;\n')
-    js_file.write('outlets = 2;\n\n')
+    js_file.write('outlets = 1;\n\n')
     # These are both necessary to make connections...the amnesia vector 'forgets' while the 'memory' one does not
     # ...see translateConnections for more
     memory = []
@@ -19,13 +19,18 @@ def fill_JS_file(filename, patch, patch_type = 'processing'):
     horizontal = [0,1]
     #vector<int> horizontal(1,0);        // keeps track of horizontal placement of each object through recursion - used when placing objects
     
+    # get dac and record as vars in js
+    js_file.write('var dac = this.patcher.getnamed("dac");\n')
+    js_file.write('var record = this.patcher.getnamed("record");\n')
+    if patch_type == 'processing':
+        js_file.write('var sfplay = this.patcher.getnamed("sfplay");\n')
     # Translate all objects in patch to javascript
     translate_objects(js_file, patch, current_level, objects, horizontal)
     # Translate all connections in patch to javascript
     translate_connections(js_file, patch, objects, memory, amnesia, patch_type)
     
     # send a bang out of the object to start audio input through the patch
-    js_file.write(" outlet(1, \"bang\");\n")
+    js_file.write(" outlet(0, \"bang\");\n")
     
     # create eraseall function to be used right before next patch loads
     js_file.write("\nfunction eraseall()\n{\n")
@@ -88,6 +93,8 @@ def convert_name(object_name):
         name = 'divide'
     elif name == '*':
         name = 'multiply'
+    elif name == 'nw.gverb':
+        name = 'nwgverb'
     return name
 
 # *** RECURSIVE ***
@@ -103,28 +110,16 @@ def translate_connections(js_file,patch,objects,memory,amnesia, patch_type):
     for i in range(0,len(patch.children)):
         # first handle case where root is a dac~
         if patch.root.name == "dac~":
-            # Tell thispatcher to name our object 'objdac' with 'i' appended (e.g. objdac4).
-            # To get the appropriate object, we need to find the total number of object's with our name
-            # and subtract the number of objects in the memory list (which is the number of object's up to this point
-            # in the tree search), because objects are referred to by this patcher in the reverse order of creation
-            js_file.write('outlet(0, "script", "nth", "objdac%d", "%s", %d);\n' % (i,patch.children[i].root.name,objects.count(convert_name(patch.children[i].root.name))-memory.count(convert_name(patch.children[i].root.name))))
-            # Now that we've associated a variable name with this object, we can create its connection to the dac and its connection to the mfcc object (that which extracts features)
-            js_file.write('outlet(0, "script", "connect", "objdac%d", %d, "dac", %d);\n' % (i,patch.connections[i].outlet, patch.connections[i].inlet))
-            js_file.write('outlet(0, "script", "connect", "objdac%d", %d, "mfcc", 0);\n' % (i,patch.connections[i].outlet))
+            js_file.write('this.patcher.connect(obj%s%d, %d, dac, %d);\n\n' % (convert_name(patch.children[i].root.name), memory.count(convert_name(patch.children[i].root.name))+1,patch.connections[i].outlet,patch.connections[i].inlet))
+            js_file.write('this.patcher.connect(obj%s%d, %d, record, %d);\n\n' % (convert_name(patch.children[i].root.name), memory.count(convert_name(patch.children[i].root.name))+1,patch.connections[i].outlet,patch.connections[i].inlet))
         # if the root is sfplay~
         elif patch.children[i].root.name == AUDIO_SOURCE and patch_type == 'processing':
-            # Tell thispatcher to name our object 'objadc' with 'i' appended. To get the appropriate object,
-            # we find the total number of object's with our name and subtract the number we have found up to 
-            # this part in the tree search and then add 1
-            js_file.write('outlet(0, "script", "nth", "objadc%d", "%s", %d);\n' % (i,patch.root.name,objects.count(convert_name(patch.root.name))-memory.count(convert_name(patch.root.name))+1))
-            # Now that we've associated a variable name with this object, we can create the appropriate connection out from sfplay~
-            js_file.write('outlet(0, "script", "connect", "sfplay", %d, "objadc%d", %d);\n' % (patch.connections[i].outlet, i, patch.connections[i].inlet))
+            js_file.write('this.patcher.connect(sfplay, %d, obj%s%d, %d);\n\n' % (patch.connections[i].outlet, convert_name(patch.root.name), amnesia.count(convert_name(patch.root.name)),patch.connections[i].inlet))
         # otherwise, simply make a connection based on the variable names created in the js file
         else:
             js_file.write('this.patcher.connect(obj%s%d, %d, obj%s%d, %d);\n\n' % (convert_name(patch.children[i].root.name), memory.count(convert_name(patch.children[i].root.name))+1,patch.connections[i].outlet, convert_name(patch.root.name), amnesia.count(convert_name(patch.root.name)),patch.connections[i].inlet))
         # translate connections for every subpatch
         translate_connections(js_file, patch.children[i], objects, memory, amnesia, patch_type)
-
 
 def translate_deletions(js_file, patch, objects):
     # leave alone default audio in and out objects, because we don't want to ever delete these
