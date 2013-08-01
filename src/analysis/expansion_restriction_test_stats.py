01510d6de21c4ab5cb68f45e6c7b3d602c1defcf
Aron Glennon <aronglennon@gmail.com>
2013-07-31 21:19:30 -0400

    src: minor updates to stats methods.

--- src/analysis/expansion_restriction_test_stats.py
+++ src/analysis/expansion_restriction_test_stats.py
@@ -1,8 +1,8 @@
-'''
-Created on Jun 16, 2013
+from mysqldb.db_commands import mysql_object
+import matplotlib.pyplot as plt
+from operator import itemgetter
 
-@author: apg250
-'''
+TESTRUN_ID = 19
 
 '''
 Parameter Set: TBD once all other tests are run
@@ -11,3 +11,18 @@ The important data we gather from tests include how the best-or-run patches’ fit
 of the function set away from only containing necessary functions and as we change the possible parameter values that functions may take, 
 as well as recording how the iteration count required to reach a best-of-run patch varies given changes in these variables.
 '''
+
+def main():
+    # NOTE: Currently, this code just outputs similarity scores sorted by some other column (e.g. total amount deleted in the sampdel tests)
+    mysql_obj = mysql_object(sameThread = True)
+    # input testrun, get back (best of run fitness, generation, patch, count)
+    values = mysql_obj.get_best_of_run(TESTRUN_ID)
+    print values
+    values = mysql_obj.get_total_num_iterations(TESTRUN_ID)
+    print values
+    values = mysql_obj.get_avg_fitness_per_generation(TESTRUN_ID)
+    print values
+    values = mysql_obj.get_max_fitness_per_generation(TESTRUN_ID)
+    print values
+    values = mysql_obj.get_min_fitness_per_generation(TESTRUN_ID)
+    print values
--- src/analysis/similarity_test_stats.py
+++ src/analysis/similarity_test_stats.py
@@ -2,13 +2,13 @@ from mysqldb.db_commands import mysql_object
 import matplotlib.pyplot as plt
 from operator import itemgetter
 
-TESTRUN_ID = 19
+TESTRUN_ID = 6
 
 def main():
     # NOTE: Currently, this code just outputs similarity scores sorted by some other column (e.g. total amount deleted in the sampdel tests)
     mysql_obj = mysql_object(sameThread = True)
-    table = 'repinsert'
-    sort_by_column = 'total_length_reps'
+    table = 'sampdel'
+    sort_by_column = 'total_deleted_content'
     values = mysql_obj.get_similarity_test_data(table, sort_by_column, TESTRUN_ID)
     DTW_Vals = []
     DPLA_Vals = []
@@ -17,13 +17,13 @@ def main():
     sortedVals = sorted(values, key=itemgetter(2))
     for v in sortedVals:
         if v[0] == 'DTW' and v[2] != 0:
-            DTW_Vals.append(1.0-5.0*(1.0-v[1]))
+            DTW_Vals.append(v[1])
         elif v[0] == 'DPLA' and v[2] != 0:
-            DPLA_Vals.append(1.0-5.0*(1.0-v[1]))
+            DPLA_Vals.append(v[1])
         elif v[0] == 'SIC-DPLA' and v[2] != 0:
-            SIC_DPLA_Vals.append(1.0-5.0*(1.0-v[1]))
+            SIC_DPLA_Vals.append(v[1])
         elif v[0] == 'euclidean' and v[2] != 0:
-            Euc_Vals.append(1.0-5.0*(1.0-v[1]))
+            Euc_Vals.append(v[1])
     plt.plot(DTW_Vals, hold = True)
     plt.plot(DPLA_Vals, hold = True)
     plt.plot(SIC_DPLA_Vals, hold = True)
