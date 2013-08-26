c3c50c737b451f2d97fff1fd7dd69e218ba0139b
Aron Glennon <aronglennon@gmail.com>
2013-08-25 21:55:09 -0400

    src: Added plotting to gen ops test analysis.

--- src/analysis/genops_test_stats.py
+++ src/analysis/genops_test_stats.py
@@ -16,6 +16,11 @@ def main():
     sortedVals = sorted(values, key=itemgetter(0))
     maxFitness = sortedVals[-1][0]
     minFitness = sortedVals[0][0]
+    xVals = []
+    yVals = []
+    for s in sortedVals:
+        xVals.append(s[0]/(maxFitness))
+        yVals.append(s[1]/(maxFitness))
     original_average_fitnesses = [0] * PARTITION_COUNT
     neighbor_average_fitnesses = [0] * PARTITION_COUNT
     count_fitnesses = [0] * PARTITION_COUNT
@@ -39,6 +44,17 @@ def main():
         if P < 0.0:
             NSC += P
     print 'NSC for testrun %d is %0.8f' % (TESTRUN_ID,NSC)
+    plt.figure(facecolor='white')
+    plt.scatter(xVals, yVals, s = 1, hold = True)
+    plt.axis(xmin = 0.0, xmax = 1.0, ymin = 0.0, ymax = 1.0)
+    for partition in range(0, PARTITION_COUNT+1):
+        plt.vlines(minFitness/(maxFitness) + (maxFitness-minFitness)*partition/((maxFitness)*(PARTITION_COUNT)), 0.0, 1.0, linestyles = ':', hold = True)
+    plt.plot([x/maxFitness for x in original_average_fitnesses], [x/maxFitness for x in neighbor_average_fitnesses], linewidth=2, color='r')
+    plt.plot([0, 1], [0, 1], linewidth=1, linestyle='--')
+    plt.title('Negative Slope Coefficient (NSC)')
+    plt.xlabel('Patch Fitness')
+    plt.ylabel('Neighbor Fitness')
+    plt.show()
     
 if __name__ == "__main__":
     main()
